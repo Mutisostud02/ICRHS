b638fede9e2cce84150063c809c73f7169e268b2
MutisoStud02 <mutiso8395@gmail.com>
2025-02-08 09:38:39 +0300

    Created 3 sections with different info for the home screen

---
+++ src/components/home.jsx
@@ -0,0 +1,41 @@
+export default function HomeOne({title, content, extraContent, src}) {
+    return (
+        <div className="group1">
+            <img src={src}/>
+            <h4>{title}</h4>
+            <p>{content}</p>
+            {extraContent && <p>{extraContent}</p>}
+        </div>
+    )
+}
+
+export function HomeTwo({title, content, stylez}) {
+    return (
+        <div className="group2">
+                <span></span>
+            <div style={stylez}>
+                <button>Donate+</button>
+                <span></span>
+            </div>
+            <h2>{title}</h2>
+            <p>{content}</p>
+            <p>Goal: $35,000</p>
+            
+        </div>
+
+    )
+}
+
+export function HomeThree({src, size, title, content, stylz}) {
+    return (
+        <div className="group3">
+            <img src={src}
+            height={size}
+            />
+            <div className="group3-info">
+            <h3>{title}</h3>
+            <p>{content}</p>
+            </div>
+        </div>
+    )
+}
